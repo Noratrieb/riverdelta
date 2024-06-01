f2a5896b9d472e37fb20812a7d58dd6bf3691fbc
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2024-06-01 14:57:09 +0200

    adjust tests

--- src/lexer.test.ts
+++ src/lexer.test.ts
@@ -7,7 +7,71 @@ it("should tokenize an emtpy function", () => {
   const tokens = tokenize(new ErrorHandler(false), { content: input });
   if (!tokens.ok) unreachable("lexer error");
 
-  expect(tokens.tokens).toMatchSnapshot();
+  expect(tokens.tokens).toMatchInlineSnapshot(`
+    [
+      {
+        "kind": "function",
+        "span": Span {
+          "end": 8,
+          "file": {
+            "content": "function hello() = ;",
+          },
+          "start": 0,
+        },
+      },
+      {
+        "ident": "hello",
+        "kind": "identifier",
+        "span": Span {
+          "end": 14,
+          "file": {
+            "content": "function hello() = ;",
+          },
+          "start": 9,
+        },
+      },
+      {
+        "kind": "(",
+        "span": Span {
+          "end": 15,
+          "file": {
+            "content": "function hello() = ;",
+          },
+          "start": 14,
+        },
+      },
+      {
+        "kind": ")",
+        "span": Span {
+          "end": 16,
+          "file": {
+            "content": "function hello() = ;",
+          },
+          "start": 15,
+        },
+      },
+      {
+        "kind": "=",
+        "span": Span {
+          "end": 18,
+          "file": {
+            "content": "function hello() = ;",
+          },
+          "start": 17,
+        },
+      },
+      {
+        "kind": ";",
+        "span": Span {
+          "end": 20,
+          "file": {
+            "content": "function hello() = ;",
+          },
+          "start": 19,
+        },
+      },
+    ]
+  `);
 });
 
 it("should tokenize hello world", () => {
@@ -16,5 +80,50 @@ it("should tokenize hello world", () => {
   const tokens = tokenize(new ErrorHandler(false), { content: input });
   if (!tokens.ok) unreachable("lexer error");
 
-  expect(tokens.tokens).toMatchSnapshot();
+  expect(tokens.tokens).toMatchInlineSnapshot(`
+    [
+      {
+        "ident": "print",
+        "kind": "identifier",
+        "span": Span {
+          "end": 5,
+          "file": {
+            "content": "print("hello world")",
+          },
+          "start": 0,
+        },
+      },
+      {
+        "kind": "(",
+        "span": Span {
+          "end": 6,
+          "file": {
+            "content": "print("hello world")",
+          },
+          "start": 5,
+        },
+      },
+      {
+        "kind": "lit_string",
+        "span": Span {
+          "end": 19,
+          "file": {
+            "content": "print("hello world")",
+          },
+          "start": 6,
+        },
+        "value": "hello world",
+      },
+      {
+        "kind": ")",
+        "span": Span {
+          "end": 20,
+          "file": {
+            "content": "print("hello world")",
+          },
+          "start": 19,
+        },
+      },
+    ]
+  `);
 });
